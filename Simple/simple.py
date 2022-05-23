e2f0100020f331e7666f26e31a2053a1be6bb458
NelsonIvan003 <nelsonivan003@gmail.com>
2022-05-23 15:26:48 -0300

    funcion crear_lista_diccionarios

--- Simple/simple.py
+++ Simple/simple.py
@@ -1,2 +1,12 @@
 import random
 import doctest
+
+
+def crear_lista_diccionarios():
+    # [{'id':1, 'edad': 'aleatorio(1,100)}]
+    lst = []
+    for i in range(10):
+        e = random.randint(1, 100)
+        lst.append({'id': i + 1, 'edad': e})
+    return lst
+
